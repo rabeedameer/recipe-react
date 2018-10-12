c3905bdaf231e3222a0a610e76701667fcb57027
rabee <rabee.dameer@gmail.com>
2018-10-12 20:52:28 +0200

    Form component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Form from './components/Form';
+
+const App = () => (
+      <div>
+        <header>
+          <h1>
+            Get your favorite recipe!
+          </h1>
+
+        </header>
+        <Form />
+      </div>
+    );
+  
+
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => (
+    <form>
+        <input type="text"/>
+        <button>search</button>
+    </form>
+);
+
+
+
+export default Form;
