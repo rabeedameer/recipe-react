cda709c9c1b50db18d252c0c14b2f3b096c9041d
rabee <rabee.dameer@gmail.com>
2018-10-12 23:21:38 +0200

    get the data desplayed on the screen

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ class App extends Component{
 
         </header>
         <Form getRecipe={this.getRecipe}/>
+        {this.state.recipes.map((recipe)=>{
+          return <p>{recipe.title}</p>
+        })}
       </div>
     );
   }
