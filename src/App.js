02db10a2552f99f45a01d707e092fe8208086708
rabee <rabee.dameer@gmail.com>
2018-10-12 22:31:55 +0200

    fetch data from an API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import Form from './components/Form';
 
+const API_KEY = "3ae8fa5d97384aa938a735f4e46b9fce";
+const baseURL = "https://www.food2fork.com/api/";
 class App extends Component{
-  getRecipe = (e) => {
+  state = {
+    recipes : []
+  }
+  getRecipe = async (e) => {
     e.preventDefault();
     const recipeName = e.target.elements.recipeName.value;
+    const apiCall =  await fetch(`${baseURL}search?key=${API_KEY}&q=chicken`);
 
-    console.log(recipeName);
+    const data = await apiCall.json(); // "this save the result of fetching tha api as json"
+    this.setState({
+      recipes : data.recipes
+    });
+    console.log(this.state.recipes);
   } 
   
   render(){
