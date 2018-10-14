e03628771c6c46f66bd5ef2dac66a4f24cc85703
rabee <rabee.dameer@gmail.com>
2018-10-14 22:19:21 +0200

    added reactstrap & tried some styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Form = (props) => (
-    <form onSubmit={props.getRecipe}>
-        <input type="text" name="recipeName"/>
-        <button>search</button>
+    <form onSubmit={props.getRecipe} style={{marginBottom: "2rem"}}>
+        <input className="" type="text" name="recipeName"/>
+        <button className="">search</button>
     </form>
 );
 
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Row, Col ,Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-
-const Recipes = (props) =>(
-    <div>
-        {props.recipes.map((recipe)=>{
+const Recipes = props => (
+    <Row>
+        {props.recipes.map((recipe) =>{
           return (
-            <div>
-              <img src={recipe.image_url} alt={recipe.recipe_title}/>
-              <p key={recipe.recipe_id}>{recipe.title}</p>
-            </div>
+            
+                <Col sm="3" key={recipe.recipe_id} >
+                    <Card key={recipe.recipe_id}>
+                        <CardImg className="cardImage" src={recipe.image_url} />
+                        <CardBody>
+                            <CardTitle>{recipe.title}</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>{recipe.title}</CardText>
+                            <Button>Show Recipe</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+            
           );
         })}
-    </div>
+    </Row>
 );
 
 
